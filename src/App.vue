8351ad2f4bad5e6ab861f9e6dd31d6821b0b069e
moali99 <mohammadali.mirzaee99@gmail.com>
2021-02-05 14:34:00 +0100

    inge ändringar nu Tack

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,11 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-<<<<<<< HEAD
     <router-view />
     <p>mohammad</p>
     <p>zeljko</p>
     <p>whatever</p>
     <p>hampus</p>
-=======
-    <router-view/>
->>>>>>> 9ab5cbd44b1791c4e1b1885d8faaaaffdc0396dd
   </div>
 </template>
 
