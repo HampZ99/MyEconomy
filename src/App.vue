7f7514df53dd6b6cd9d50c31cb62297c74845faf
moali99 <mohammadali.mirzaee99@gmail.com>
2021-02-05 13:46:32 +0100

    hej jag är mohammad

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,8 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <router-view />
+    <p>mohammad</p>
   </div>
 </template>
 
