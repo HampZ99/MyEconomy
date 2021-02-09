d0486cd55a8e1b309ca5ccc54da5d2c090fbe8de
Leo <lenti11@live.se>
2021-02-09 19:27:11 +0100

    Added link to history

--- src/App.vue
+++ src/App.vue
@@ -82,6 +82,7 @@
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
+    background: #fafafa;
     padding: 0;
     margin: 0;
   }
