8e98cb515e70ad25cef738132909d6865e2520c2
moali99 <mohammadali.mirzaee99@gmail.com>
2021-02-09 13:43:57 +0100

    login page

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link>
+      <router-link to="/">Home</router-link>|
+      <router-link to="/Login">Login</router-link>
     </div>
     <router-view />
   </div>
 </template>
 
 <style>
+html,
+body {
+  padding: 0;
+  margin: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
