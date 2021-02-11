205630e89e9fa2e80a324dc6289683865f2a2d54
Pob74 <70943669+Pob74@users.noreply.github.com>
2021-02-11 11:35:03 +0100

    Color change Stocks.vue

--- src/views/Stocks.vue
+++ src/views/Stocks.vue
@@ -2,7 +2,7 @@
   <div class="Stocks">
           <Header title="Dashboard" desc="Welcome Back" :date="true"/>
     <div class="table">
-        <b-table  hover bordered :items="items"></b-table>
+        <b-table class="banner"  hover bordered table-responsive :items="items"></b-table>
     </div>
     <div class="total">
         <h3>Total winst or loss  <span><input type="column"></span></h3>
@@ -20,11 +20,11 @@ export default {
     data() {
         return {
             items: [
-                { name: 'Investor B', stocks_owned: '5', stocks_price: 10, stocks_trend_1year: '10', stocks_trend_24h: '10', _cellVariants: { name: 'info', stocks_owned: 'warning', stocks_price: 'warning', stocks_trend_1year: 'warning',stocks_trend_24h: 'warning' } },
-          { name: 'Hennes & Mauritz AB', stocks_owned: '5', stocks_price: '10', stocks_trend_1year: '10', stocks_trend_24h: '10', _cellVariants: { name: 'info', stocks_owned: 'warning', stocks_price: 'warning', stocks_trend_1year: 'warning',stocks_trend_24h: 'warning' } }, 
-          { name: 'Kinnevik B', stocks_owned: '5', stocks_price: '10', stocks_trend_1year: '10', stocks_trend_24h: '10', _cellVariants: { name: 'info', stocks_owned: 'warning', stocks_price: 'warning', stocks_trend_1year: 'warning',stocks_trend_24h: 'warning' } }, 
-          { name: 'Volvo B', stocks_owned: '5', stocks_price: '10', stocks_trend_1year: '10', stocks_trend_24h: '10', _cellVariants: { name: 'info', stocks_owned: 'warning', stocks_price: 'warning', stocks_trend_1year: 'warning',stocks_trend_24h: 'warning' } }, 
-          { name: 'Swedbank A', stocks_owned: '5', stocks_price: '10', stocks_trend_1year: '10', stocks_trend_24h: '10', _cellVariants: { name: 'info', stocks_owned: 'warning', stocks_price: 'warning', stocks_trend_1year: 'warning',stocks_trend_24h: 'warning' } } 
+                { name: 'Investor B', stocks_owned: '5', stocks_price: 10, stocks_trend_1year: '10', stocks_trend_24h: '10', _cellVariants: { name: 'dark', stocks_owned: 'secondary', stocks_price: 'primary', stocks_trend_1year: 'secondary',stocks_trend_24h: 'primary' } },
+          { name: 'Hennes & Mauritz AB', stocks_owned: '5', stocks_price: '10', stocks_trend_1year: '10', stocks_trend_24h: '10', _cellVariants: { name: 'dark', stocks_owned: 'secondary', stocks_price: 'primary', stocks_trend_1year: 'secondary',stocks_trend_24h: 'primary' } }, 
+          { name: 'Kinnevik B', stocks_owned: '5', stocks_price: '10', stocks_trend_1year: '10', stocks_trend_24h: '10', _cellVariants: { name: 'dark', stocks_owned: 'secondary', stocks_price: 'primary', stocks_trend_1year: 'secondary',stocks_trend_24h: 'primary' } }, 
+          { name: 'Volvo B', stocks_owned: '5', stocks_price: '10', stocks_trend_1year: '10', stocks_trend_24h: '10', _cellVariants: { name: 'dark', stocks_owned: 'secondary', stocks_price: 'primary', stocks_trend_1year: 'secondary',stocks_trend_24h: 'primary' } }, 
+          { name: 'Swedbank A', stocks_owned: '5', stocks_price: '10', stocks_trend_1year: '10', stocks_trend_24h: '10', _cellVariants: { name: 'dark', stocks_owned: 'secondary', stocks_price: 'primary', stocks_trend_1year: 'secondary',stocks_trend_24h: 'primary' } } 
         ]
       }
     }
@@ -42,4 +42,9 @@ export default {
     margin-right: 10px;
     margin-top: 30px;
 }
+table.table tr.table-foobar td {
+  background-color: rgb(156, 21, 44) ;
+}
+
+
 </style>
