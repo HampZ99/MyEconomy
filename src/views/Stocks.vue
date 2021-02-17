3c8c73e30f0c0ed73f8bcb3e7b3aeb6888be5ba0
Pob74 <70943669+Pob74@users.noreply.github.com>
2021-02-17 11:28:29 +0100

    Added Buy/Sell to test

--- src/views/Stocks.vue
+++ src/views/Stocks.vue
@@ -8,7 +8,9 @@
         <b-pagination v-model="currentPage" :total-rows="rows" :per-page="perPage"></b-pagination>
     </div>
     <div class="total">
-        <h3>Total winst or loss  <span><input type="column" :value='total'></span></h3>
+        <h3>Total winst or loss 24h  <span><input type="column" :value='total'></span></h3>
+       
+        
     </div>
     <div class="search">
         <b-row>
@@ -35,15 +37,15 @@ export default {
             filter:'',
             
             items: [
-                { name: 'Investor B', stocks_owned: 5, stocks_price: 10, stocks_trend_1year: 10, stocks_trend_24h: 60, _cellVariants: { name: 'dark', stocks_owned: 'secondary', stocks_price: 'primary', stocks_trend_1year: 'secondary',stocks_trend_24h: 'primary' } },
-          { name: 'Hennes & Mauritz AB', stocks_owned: 5, stocks_price: 10, stocks_trend_1year: 10, stocks_trend_24h: 10, _cellVariants: { name: 'dark', stocks_owned: 'secondary', stocks_price: 'primary', stocks_trend_1year: 'secondary',stocks_trend_24h: 'primary' } }, 
-          { name: 'Kinnevik B', stocks_owned: 5, stocks_price: 10, stocks_trend_1year: 10, stocks_trend_24h: 10, _cellVariants: { name: 'dark', stocks_owned: 'secondary', stocks_price: 'primary', stocks_trend_1year: 'secondary',stocks_trend_24h: 'primary' } }, 
-          { name: 'Volvo B', stocks_owned: 5, stocks_price: 10, stocks_trend_1year: 10, stocks_trend_24h: 10, _cellVariants: { name: 'dark', stocks_owned: 'secondary', stocks_price: 'primary', stocks_trend_1year: 'secondary',stocks_trend_24h: 'primary' } }, 
-          { name: 'Swedbank A', stocks_owned: 5, stocks_price: 10, stocks_trend_1year: 10, stocks_trend_24h: 10, _cellVariants: { name: 'dark', stocks_owned: 'secondary', stocks_price: 'primary', stocks_trend_1year: 'secondary',stocks_trend_24h: 'primary' } },
-          { name: 'SAS', stocks_owned: 5, stocks_price: 10, stocks_trend_1year: 10, stocks_trend_24h: 10, _cellVariants: { name: 'dark', stocks_owned: 'secondary', stocks_price: 'primary', stocks_trend_1year: 'secondary',stocks_trend_24h: 'primary' } },
-          { name: 'Latour B', stocks_owned: 5, stocks_price: 10, stocks_trend_1year: 10, stocks_trend_24h: 10, _cellVariants: { name: 'dark', stocks_owned: 'secondary', stocks_price: 'primary', stocks_trend_1year: 'secondary',stocks_trend_24h: 'primary' } },
-          { name: 'Embracer Group B', stocks_owned: 5, stocks_price: 10, stocks_trend_1year: 10, stocks_trend_24h: 10, _cellVariants: { name: 'dark', stocks_owned: 'secondary', stocks_price: 'primary', stocks_trend_1year: 'secondary',stocks_trend_24h: 'primary' } },
-           { name: 'Axfood', stocks_owned: 5, stocks_price: 10, stocks_trend_1year: 10, stocks_trend_24h: 10, _cellVariants: { name: 'dark', stocks_owned: 'secondary', stocks_price: 'primary', stocks_trend_1year: 'secondary',stocks_trend_24h: 'primary' } }   
+                { name: 'Investor B', stocks_owned: 15, stocks_price: 123, stocks_trend_1year: +10, stocks_trend_24h: -60, actions:' Buy / Sell', _cellVariants: { name: 'dark', stocks_owned: 'secondary', stocks_price: 'primary', stocks_trend_1year: 'secondary',stocks_trend_24h: 'primary', actions: 'danger' } },
+          { name: 'Hennes & Mauritz AB', stocks_owned: 80, stocks_price: 306, stocks_trend_1year: 2590, stocks_trend_24h: 59, actions: ' Buy / Sell', _cellVariants: { name: 'dark', stocks_owned: 'secondary', stocks_price: 'primary', stocks_trend_1year: 'secondary',stocks_trend_24h: 'primary', actions: 'danger' } }, 
+          { name: 'Kinnevik B', stocks_owned: 106, stocks_price: 103, stocks_trend_1year: -2210, stocks_trend_24h: -16, actions: ' Buy / Sell', _cellVariants: { name: 'dark', stocks_owned: 'secondary', stocks_price: 'primary', stocks_trend_1year: 'secondary',stocks_trend_24h: 'primary', actions: 'danger' } }, 
+          { name: 'Volvo B', stocks_owned: 12, stocks_price: 125, stocks_trend_1year: 1256, stocks_trend_24h: -156, actions: ' Buy / Sell', _cellVariants: { name: 'dark', stocks_owned: 'secondary', stocks_price: 'primary', stocks_trend_1year: 'secondary',stocks_trend_24h: 'primary', actions: 'danger' } }, 
+          { name: 'Swedbank A', stocks_owned: 86, stocks_price: 230, stocks_trend_1year: 459, stocks_trend_24h: 56, actions: ' Buy / Sell', _cellVariants: { name: 'dark', stocks_owned: 'secondary', stocks_price: 'primary', stocks_trend_1year: 'secondary',stocks_trend_24h: 'primary', actions: 'danger' } },
+          { name: 'SAS', stocks_owned: 29, stocks_price: 415, stocks_trend_1year: -45, stocks_trend_24h: 95, actions: ' Buy / Sell', _cellVariants: { name: 'dark', stocks_owned: 'secondary', stocks_price: 'primary', stocks_trend_1year: 'secondary',stocks_trend_24h: 'primary', actions: 'danger' } },
+          { name: 'Latour B', stocks_owned: 156, stocks_price: 156, stocks_trend_1year: 1562, stocks_trend_24h: 1.3, actions: ' Buy / Sell', _cellVariants: { name: 'dark', stocks_owned: 'secondary', stocks_price: 'primary', stocks_trend_1year: 'secondary',stocks_trend_24h: 'primary', actions: 'danger' } },
+          { name: 'Embracer Group B', stocks_owned: 45, stocks_price: 94, stocks_trend_1year: 4586, stocks_trend_24h: 12.9, actions: ' Buy / Sell', _cellVariants: { name: 'dark', stocks_owned: 'secondary', stocks_price: 'primary', stocks_trend_1year: 'secondary',stocks_trend_24h: 'primary', actions: 'danger' } },
+           { name: 'Axfood', stocks_owned: 48, stocks_price: 456, stocks_trend_1year: -4569, stocks_trend_24h: 12, actions: ' Buy / Sell', _cellVariants: { name: 'dark', stocks_owned: 'secondary', stocks_price: 'primary', stocks_trend_1year: 'secondary',stocks_trend_24h: 'primary', actions: 'danger' } }   
         ]
       }
     },
@@ -58,6 +60,7 @@ export default {
         0
       )
     }
+   
        
     }
 }
@@ -77,6 +80,10 @@ export default {
     float: left;
     margin-top: 30px;
 }
+input {
+    text-align: center;
+}
+
 
 </style>
     
